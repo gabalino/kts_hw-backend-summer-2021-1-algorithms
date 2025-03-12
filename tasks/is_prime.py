7f659f6b3558bd7d1bc4633b37de1b0c140525ab
Aleksandr Khokhlov <hohlovap@gmail.com>
2025-03-12 19:24:52 +0300

    add function seconds_is_prime()

--- tasks/is_prime.py
+++ tasks/is_prime.py
@@ -1,5 +1,6 @@
 __all__ = ("is_prime",)
 
+import math
 
 def is_prime(number: int) -> bool:
     """Определяет, является ли число простым.
@@ -12,4 +13,17 @@ def is_prime(number: int) -> bool:
         >> is_prime(4):
         True
     """
-    raise NotImplementedError
+    result = False
+    if number > 2:
+        square_root = int(math.sqrt(number))
+        count = 0
+        for i in range(2, square_root+1):
+            if number % i == 0:
+                count += 1
+                if count > 1:
+                    break
+        if count == 0:
+            result = True
+    elif number == 2:
+        result = True
+    return result
