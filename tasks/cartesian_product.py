5c2606a3625a82c834b69379c96ee8b35cd3bd0c
Aleksandr Khokhlov <hohlovap@gmail.com>
2025-03-16 09:14:06 +0300

    add function cartesian_product

--- tasks/cartesian_product.py
+++ tasks/cartesian_product.py
@@ -14,4 +14,10 @@ def cartesian_product(arr1: list[T1], arr2: list[T2]) -> list[tuple[T1, T2]]:
         >> cartesian_product([1, 2], [3, 4])
         [(1, 3), (1, 4), (2, 3), (2, 4)]
     """
-    raise NotImplementedError
+    result = []
+
+    for i in range(len(arr1)):
+        for j in range(len(arr2)):
+            result.append((arr1[i], arr2[j]))
+
+    return result
