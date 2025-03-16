8298b5c12e08b33ab27a3fa9c5a0fcac91607c2e
Aleksandr Khokhlov <hohlovap@gmail.com>
2025-03-16 17:49:46 +0300

    add function even_odd

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -9,4 +9,18 @@ def even_odd(numbers: list[int]) -> float:
         >> even_odd([1, 2, 3, 4, 5])
         0.6667
     """
-    raise NotImplementedError
+    result = 0
+    even = None
+    odd = None
+    length = len(numbers)
+    even = sum(numbers[i] for i in range(0, length) if i % 2 == 1)
+    odd = sum(numbers[i] for i in range(0 ,length) if i % 2 == 0)
+
+    if length >= 2:
+        if odd != 0:
+            result = even / odd
+
+
+    print(f"{even} \ {odd} = {result}")
+    return result
+
